a1bb915c8f84fe14ea0764581d25348481699180
mwu <mwu-tow@gazeta.pl>
2019-03-27 17:15:14 +0100

    use simple build type

--- Setup.hs
+++
@@ -1,6 +0,0 @@
-import Distribution.Simple
-import System.Cmd(system)
-
-main = defaultMainWithHooks $ simpleUserHooks { runTests = runPecoffTests }
-
-runPecoffTests a b pd lb = system "runhaskell -i./src ./tests/Test.hs" >> return ()
