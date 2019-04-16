e784b7747e2e0133ac4e0e2478da16a3eee543de
mwu <mwu-tow@gazeta.pl>
2019-04-16 18:40:15 +0200

    minor utf8-reading cleanup

--- src/Data/Pecoff/Gettable.hs
+++ src/Data/Pecoff/Gettable.hs
@@ -51,49 +51,52 @@ instance Gettable Int64 where
 instance Gettable UTCTime where
     get = (posixSecondsToUTCTime . fromInteger . fromIntegral) <$> getWord32le
 
+-- | Deserialize UTF-8 encoded character (taking 1 - 4 bytes)
+instance Gettable Char where
+    get = do
+        let getByte = liftM fromIntegral getWord8 :: Get Word32
+        b1 <- getByte
+        let getCharUTF82 = do
+                b2 <- getByte
+                if b2 .&. 0xc0 == 0x80 
+                    then return $ ((b1 .&. 0x1f) `shiftL` 6) 
+                               .|. (b2 .&. 0x3f)
+                    else fail "Invalid second byte in UTf8 string."
+            getCharUTF83 = do
+                b2 <- getByte
+                b3 <- getByte
+                if b2 .&. 0xc0 == 0x80 && b3 .&. 0xc0 == 0x80 
+                    then return $ ((b1 .&. 0x0f) `shiftL` 12)
+                              .|. ((b2 .&. 0x3f) `shiftL` 6) 
+                              .|.  (b3 .&. 0x3f)
+                    else fail "Invalid second or third byte in UTf8 string."
+            getCharUTF84 = do
+                b2 <- getByte
+                b3 <- getByte
+                b4 <- getByte
+                if b2 .&. 0xc0 == 0x80 && b3 .&. 0xc0 == 0x80 && b4 .&. 0xc0 == 0x80 
+                    then return $ ((b1 .&. 0x07) `shiftL` 18)
+                              .|. ((b2 .&. 0x3f) `shiftL` 12)
+                              .|. ((b3 .&. 0x3f) `shiftL` 6) 
+                              .|.  (b4 .&. 0x3f)
+                    else fail "Invalid second or third or fourth byte in UTf8 string."
+        codePoint <- case b1 of
+            n | n .&. 0x80 == 0x00 -> return n
+            n | n .&. 0xe0 == 0xc0 -> getCharUTF82
+            n | n .&. 0xf0 == 0xe0 -> getCharUTF83
+            n | n .&. 0xf8 == 0xf0 -> getCharUTF84
+            _                      -> fail "Invalid first byte in UTF8 string."
+        pure $ chr $ fromIntegral codePoint
+    
+
 -- | Deserialize UTF-8 encoded string, terminated by null character or input end
 instance Gettable String where
-    get = liftM (map (chr . fromIntegral)) getUtf8String_
-        where getUtf8String_ = do
-                empty <- isEmpty
-                if empty then
-                    return []
-                else do
-                    char <- getCharUTF8
-                    if char == 0 then
-                        return []
-                    else do
-                        rest <- getUtf8String_
-                        return (char : rest)
-
--- | Deserialize a single UTF-8 encoded codepoint from binary
-getCharUTF8 :: Get Word32
-getCharUTF8 = do
-  let getCharUTF82 b1 = do
-        b2 <- liftM fromIntegral getWord8 :: Get Word32
-        if b2 .&. 0xc0 == 0x80 then
-          return $ ((b1 .&. 0x1f) `shiftL` 6) .|. (b2 .&. 0x3f)
-         else
-          fail "Invalid second byte in UTf8 string."
-      getCharUTF83 b1 = do
-        b2 <- liftM fromIntegral getWord8 :: Get Word32
-        b3 <- liftM fromIntegral getWord8 :: Get Word32
-        if b2 .&. 0xc0 == 0x80 && b3 .&. 0xc0 == 0x80 then
-          return $ ((b1 .&. 0x0f) `shiftL` 12) .|. ((b2 .&. 0x3f) `shiftL` 6) .|. (b3 .&. 0x3f)
-         else
-          fail "Invalid second or third byte in UTf8 string."
-      getCharUTF84 b1 = do
-        b2 <- liftM fromIntegral getWord8 :: Get Word32
-        b3 <- liftM fromIntegral getWord8 :: Get Word32
-        b4 <- liftM fromIntegral getWord8 :: Get Word32
-        if b2 .&. 0xc0 == 0x80 && b3 .&. 0xc0 == 0x80 && b4 .&. 0xc0 == 0x80 then
-          return $ ((b1 .&. 0x07) `shiftL` 18) .|. ((b2 .&. 0x3f) `shiftL` 12) .|. ((b3 .&. 0x3f) `shiftL` 6) .|. (b4 .&. 0x3f)
-         else
-          fail "Invalid second or third byte in UTf8 string."
-  b1 <- liftM fromIntegral getWord8 :: Get Word32
-  case b1 of
-    n | n .&. 0x80 == 0x00 -> return $ fromIntegral n
-    n | n .&. 0xe0 == 0xc0 -> getCharUTF82 n
-    n | n .&. 0xf0 == 0xe0 -> getCharUTF83 n
-    n | n .&. 0xf8 == 0xf0 -> getCharUTF84 n
-    _                      -> fail "Invalid first byte in UTF8 string."
+    get = do
+        empty <- isEmpty
+        if empty 
+            then pure []
+            else do
+                char <- get
+                if char == '\0'
+                    then pure []
+                    else (char :) <$> get
